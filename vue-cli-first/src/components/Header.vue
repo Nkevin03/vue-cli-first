bac3b923a85e6e3996c7debc41c24d9b8bab37b9
Nkevin03 <knezosi6@gmail.com>
2021-11-22 15:08:31 +0100

    Commit title

---
+++ vue-cli-first/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <h1>La mia prima app con vue CLI</h1>
+    </header>
+</template>
+
+<script>
+export default {
+     name: 'header',
+}
+</script>
+
+<style>
+    header{
+        background-color: #404eed;
+        padding: 20px;
+    }
+
+    header h1{
+        color: #fff;
+    }
+</style>
